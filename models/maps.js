f96ea67818cb9d450cdf1265f3481ef836618053
sahbana <sahbanalo@gmail.com>
2016-11-11 06:13:33 +0700

    modify map schema structur to be one document one supervisor

--- models/maps.js
+++ models/maps.js
@@ -6,31 +6,28 @@ const mapSchema =  new Schema({
     type: Schema.Types.ObjectId,
     ref: 'users'
   },
-  businessName: String,
-  pinDrop: [
+  businessName: String,  
+  pinDropName: String,
+  position: {
+    lat: String,
+    lng: String
+  },
+  supervisor: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  inputTime: Date,
+  listField: [
     {
-      pinDropName: String,
-      position: {
-        lat: String,
-        lng: String
-      },
-      supervisor: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      },
-      inputTime: Date,
-      listField: [
-        {
-          fieldName: String, // Field Name
-          fieldType: String, // Field type [Number]
-          isPass: Boolean, // Pass status [true, false]
-          targetValue: Number, // 50000
-          targetComparison: String, // [gt, lt]
-          value: Number // 45000
-        }
-      ]
+      fieldName: String, // Field Name
+      fieldType: String, // Field type [Number]
+      isPass: Boolean, // Pass status [true, false]
+      targetValue: Number, // 50000
+      targetComparison: String, // [gt, lt]
+      value: Number // 45000
     }
   ]
+
 }, {
   timestamps: true
 })
