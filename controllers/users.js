34fb4c5176142eb1dd1ce99d1a6fdb3d0d847efb
sahbana <sahbanalo@gmail.com>
2016-11-11 10:52:21 +0700

    forgot password controller first prototype added

---
+++ controllers/users.js
@@ -0,0 +1,148 @@
+var async = require('async');
+var crypto = require('crypto');
+var nodemailer = require('nodemailer');
+var passport = require('passport');
+var User = require('../models/users');
+
+/**
+ * GET /forgot
+ */
+exports.forgotGet = function(req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  res.render('account/forgot', {
+    title: 'Forgot Password'
+  });
+};
+
+/**
+ * POST /forgot
+ */
+exports.forgotPost = function(req, res, next) {
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('email', 'Email cannot be blank').notEmpty();
+  req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('error', errors);
+    return res.redirect('/forgot');
+  }
+
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(16, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', { msg: 'The email address ' + req.body.email + ' is not associated with any account.' });
+          return res.redirect('/forgot');
+        }
+        user.passwordResetToken = token;
+        user.passwordResetExpires = Date.now() + 3600000; // expire in 1 hour
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      //////
+
+      // create reusable transporter object using the default SMTP transport
+      var transporter = nodemailer.createTransport('smtps://mapinczero%40gmail.com:password@smtp.gmail.com');
+      // setup e-mail data with unicode symbols
+      var mailOptions = {
+          from: '"Map Inc. 👥" <mapinczero@gmail.com>', // sender address
+          to: user.email, // list of receivers
+          subject: 'teset ✔', // Subject line
+          subject: '✔ Reset your password on Mapinc',
+          text: 'You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+          // html: '<b>Hello world 🐴</b>' // html body
+      };
+
+      transporter.sendMail(mailOptions, function(err) {
+        req.flash('info', { msg: 'An email has been sent to ' + user.email + ' with further instructions.' });
+        res.redirect('/forgot');
+      });
+    }
+  ]);
+};
+
+/**
+ * GET /reset
+ */
+exports.resetGet = function(req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  User.findOne({ passwordResetToken: req.params.token })
+    .where('passwordResetExpires').gt(Date.now())
+    .exec(function(err, user) {
+      if (!user) {
+        req.flash('error', { msg: 'Password reset token is invalid or has expired.' });
+        return res.redirect('/forgot');
+      }
+      res.render('account/reset', {
+        title: 'Password Reset'
+      });
+    });
+};
+
+/**
+ * POST /reset
+ */
+exports.resetPost = function(req, res, next) {
+  req.assert('password', 'Password must be at least 4 characters long').len(4);
+  req.assert('confirm', 'Passwords must match').equals(req.body.password);
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('error', errors);
+    return res.redirect('back');
+  }
+
+  async.waterfall([
+    function(done) {
+      User.findOne({ passwordResetToken: req.params.token })
+        .where('passwordResetExpires').gt(Date.now())
+        .exec(function(err, user) {
+          if (!user) {
+            req.flash('error', { msg: 'Password reset token is invalid or has expired.' });
+            return res.redirect('back');
+          }
+          user.password = req.body.password;
+          user.passwordResetToken = undefined;
+          user.passwordResetExpires = undefined;
+          user.save(function(err) {
+            req.logIn(user, function(err) {
+              done(err, user);
+            });
+          });
+        });
+    },
+    function(user, done) {
+      var transporter = nodemailer.createTransport('smtps://mapinczero%40gmail.com:password@smtp.gmail.com');
+      var mailOptions = {
+        from: 'mapinczero@gmail.com',
+        to: user.email,
+        subject: 'Your password has been changed',
+        text: 'Hello,\n\n' +
+        'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+      };
+      transporter.sendMail(mailOptions, function(err) {
+        req.flash('success', { msg: 'Your password has been changed successfully.' });
+        res.redirect('/account');
+      });
+    }
+  ]);
+};
