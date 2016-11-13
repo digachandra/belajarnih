97788b987f78b4318dd510860d2e1ce7a0d87b6a
sahbana <sahbanalo@gmail.com>
2016-11-13 23:30:56 +0700

    add routes for reset and forgot password in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 const apiSupervisor = require('./apisupervisor')
 const apiSeeding = require('./api.seeding')
+const UserController = require('../controllers/users')
 
 router.use('/api/supervisor', apiSupervisor)
 router.use('/api/seeding', apiSeeding)
+router.get('/forgot',UserController.forgotGet)
+router.post('/forgot',UserController.forgotPost)
+router.get('/reset/:token',UserController.resetGet)
+router.post('/reset/:token',UserController.resetPost)
 
 module.exports = router
