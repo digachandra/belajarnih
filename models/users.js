fffd57d78ff9a3a5babb1e0e7d802de607f08f11
digachandra <imdigachandra@gmail.com>
2016-11-13 10:13:13 +0700

    Fix conflict in user model

--- models/users.js
+++ models/users.js
@@ -4,12 +4,9 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
   userEmail: { type: String, unique: true},
   encryptedPassword: String,
-<<<<<<< HEAD
   passwordResetToken: String,
   passwordResetExpires: Date,
   role: [Number]
-=======
->>>>>>> 67de6ac723e10e0787cc97a69c4cda3ca405ebb1
 })
 
 const Users = mongoose.model('users', userSchema)
