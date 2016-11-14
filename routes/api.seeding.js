7c8049c537e6044ff571e0b2531736820d1cdcf3
Andrew <atandiawan@gmail.com>
2016-11-14 18:27:08 +0700

    seeding updated

--- routes/api.seeding.js
+++ routes/api.seeding.js
@@ -3,59 +3,13 @@ const router = express.Router()
 const Users = require('../models/users.js')
 const Maps = require('../models/maps.js')
 
-router.get('/set', function(req,res){
-  let newuser  = new Users({userEmail: "andrew@andrew.com", encryptedPassword: "halhlahlha"})
-  let newspv  = new Users({userEmail: "inispv@andrew.com", encryptedPassword: "halhlahlha"})
-  newuser.save(function(err,result){
-    newspv.save(function(err,result4){
-      let newmap = new Maps({owner: newuser._id, businessName: "halo", pinDropName: "BranchMana", position: {lat: "8", lng: "7"}, supervisor: newspv._id, inputTime: new Date()})
-      newmap.listField.push({fieldName: "sales", fieldType: "number", targetValue: 700, isPass: false, targetComparsion: "gt"})
-      newmap.save(function(err2,result2){
-        let newmap2 = new Maps({owner: newuser._id, businessName: "halo", pinDropName: "BranchMana 2", position: {lat: "8", lng: "7"}, supervisor: newspv._id, inputTime: new Date()})
-        newmap2.listField.push({fieldName: "sales", fieldType: "number", targetValue: 700, isPass: false, targetComparsion: "gt" })
-        newmap2.save(function(err3,result3){
-          res.json({message: "seed user berhasil"})
-        })
-      })
-    })
-  })
-})
-
 router.get('/get', function(req,res){
   Maps.find({}).populate('owner').populate('supervisor').exec(function(err,result){
     res.json(result)
   })
 })
 
-router.get('/tambahbisnismakanan', function(req,res){
-  let spv_id = "58294c019c17424e7eb63886"
-  let owner_id = "58294c019c17424e7eb63885"
-  let newmap = new Maps({owner: owner_id, businessName: "makanan", pinDropName: "Branch 6", position: {lat: "8", lng: "7"}, supervisor: spv_id, inputTime: new Date()})
-  newmap.listField.push({fieldName: "sales", fieldType: "number", targetValue: 700, isPass: false, targetComparsion: "gt"})
-  newmap.save(function(err2,result2){
-    let newmap2 = new Maps({owner: owner_id, businessName: "makanan", pinDropName: "Branch 6", position: {lat: "8", lng: "7"}, supervisor: spv_id, inputTime: new Date()})
-    newmap2.listField.push({fieldName: "sales", fieldType: "number", targetValue: 700, isPass: false, targetComparsion: "gt" })
-    newmap2.save(function(err3,result3){
-      res.json({message: "seed user berhasil"})
-    })
-  })
-})
-
-router.get('/tambahbisnispengiriman', function(req,res){
-  let spv_id = "58294c019c17424e7eb63886"
-  let owner_id = "58294c019c17424e7eb63885"
-  let newmap = new Maps({owner: owner_id, businessName: "pengiriman", pinDropName: "Branch Pengiriman 1", position: {lat: "8", lng: "7"}, supervisor: spv_id, inputTime: new Date()})
-  newmap.listField.push({fieldName: "sales", fieldType: "number", targetValue: 700, isPass: false, targetComparsion: "gt"})
-  newmap.save(function(err2,result2){
-    let newmap2 = new Maps({owner: owner_id, businessName: "pengiriman", pinDropName: "Branch Pengiriman 2", position: {lat: "8", lng: "7"}, supervisor: spv_id, inputTime: new Date()})
-    newmap2.listField.push({fieldName: "sales", fieldType: "number", targetValue: 700, isPass: false, targetComparsion: "gt" })
-    newmap2.save(function(err3,result3){
-      res.json({message: "seed user berhasil"})
-    })
-  })
-})
-
-router.get('/seedingcomplete', function(req,res){
+router.get('/set', function(req,res){
   let newowner  = new Users({userEmail: "andrew@businessowner.com", encryptedPassword: "bukanencryptedpassword"})
   let newspv  = new Users({userEmail: "andrew@businesssupervisor.com", encryptedPassword: "jugabukanencryptedpassword"})
 
--- routes/api.supervisor.js
+++ routes/api.supervisor.js
@@ -22,7 +22,7 @@ router.post('/postdata', function(req,res){
 })
 
 router.get('/getownerlist', function(req,res){
-  let user_id = "58299642754d4310e7dd60d4" //later change with req.session
+  let user_id = "58299ad3baff8813d5911300" //later change with req.session
   Maps.find({supervisor: user_id}).populate("owner").exec(function(err,owners){
     res.json(owners)
   })
@@ -30,21 +30,21 @@ router.get('/getownerlist', function(req,res){
 
 
 router.get('/getbusinesslist/:owner_id', function(req,res){
-  let user_id = "58299642754d4310e7dd60d4" //later change with req.session
+  let user_id = "58299ad3baff8813d5911300" //later change with req.session
   Maps.find({supervisor: user_id}).populate("owner").exec(function(err,businesses){
     res.json(businesses)
   })
 })
 
 router.get('/getpinlist/:businessname', function(req,res){
-  let user_id = "58299642754d4310e7dd60d4" //later change with req.session
+  let user_id = "58299ad3baff8813d5911300" //later change with req.session
   Maps.find({businessName:req.params.businessname, supervisor: user_id}, function(err,pin){
     res.json(pin)
   })
 })
 
 router.get('/getpindate/', function(req,res){
-  let user_id = "58299642754d4310e7dd60d4"
+  let user_id = "58299ad3baff8813d5911300"
   Maps.find({supervisor: user_id, owner: req.query.ownerid, businessName: req.query.businessname, pinDropName: req.query.pindropname, "listField.value": null }, function(err,pin){
     console.log(pin)
     res.json(pin)
