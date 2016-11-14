a8a5cc66b7549caa54f494b7f77883ce410df8b7
sahbana <sahbanalo@gmail.com>
2016-11-14 23:21:43 +0700

    forgot password fix flash message, and install process.env package

--- public/javascripts/forgot.password.js
+++ public/javascripts/forgot.password.js
@@ -5,26 +5,18 @@ var ForgotPasswordForm = React.createClass({
   inputEmailChange(e){
     this.setState({email: e.target.value})
   },
-  handleFormSubmit(e){
-    $.ajax({
-      url: '/user/forgot',
-      dataType: 'json',
-      type: 'POST',
-      data: {email: this.state.email}
-    })
-  },
   render: function(){
     return(
       <div className="panel">
         <div className="panel-body">
-          <form onSubmit={this.handleFormSubmit}>
+          <form method='POST'>
             <h3>Forgot Password</h3>
             <div className="form-group">
               <label htmlFor="email">Email</label>
               <input type="email" name="email" id="email" placeholder="your@email.com" autofocus className="form-control" onChange = {this.inputEmailChange}/>
             </div>
             <div className="form-group">
-              <button type="submit" className="btn btn-success">Request Reset Password </button>
+              <input type="submit" className="btn btn-success" value='Request Reset Password'/>
             </div>
           </form>
         </div>
