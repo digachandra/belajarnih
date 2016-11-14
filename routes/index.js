8835aa70e559ecde7cc2ef6d4ab170379747ee2a
ivangerard <ivan.gerard@hotmail.com>
2016-11-14 12:16:29 +0700

    Merged model

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 const apiSupervisor = require('./apisupervisor')
 const apiSeeding = require('./api.seeding')
+const user = require('./user')
+
 
 router.use('/api/supervisor', apiSupervisor)
 router.use('/api/seeding', apiSeeding)
+router.use('/api/user', user)
+
+
 
 module.exports = router
