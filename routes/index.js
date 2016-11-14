20a1500f40cd67e4dc910b1631c1d3db1cddb615
digachandra <imdigachandra@gmail.com>
2016-11-14 09:09:45 +0700

    Add page map routing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const pageMap = require('./page.map')
 const apiSupervisor = require('./api.supervisor')
 const apiSeeding = require('./api.seeding')
 const UserController = require('../controllers/users')
@@ -10,5 +11,7 @@ router.get('/forgot',UserController.forgotGet)
 router.post('/forgot',UserController.forgotPost)
 router.get('/reset/:token',UserController.resetGet)
 router.post('/reset/:token',UserController.resetPost)
+
+router.use('/map', pageMap)
 
 module.exports = router
