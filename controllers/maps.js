2daa9cfcba1f683e154a789b59895c7f1a3bd4e1
ivangerard <ivan.gerard@hotmail.com>
2016-11-15 11:49:50 +0700

    Added end point Listpin,selectpin

--- controllers/maps.js
+++ controllers/maps.js
@@ -1,14 +1,21 @@
 var Maps = require ('../models/maps')
 
 module.exports = {
-  selectPin:selectPin
+  listPin:listPin,
+  getDetails:getDetails
 }
 
 
-function selectPin(req,res,next){
-  let ownerId = "58299ad3baff8813d5911300"
-    Maps.find({owner:ownerId},(err,data) => {
+function listPin(req,res,next){
+    Maps.find({},(err,data) => {
       res.json(data)
+    })
+}
 
+
+function getDetails(req,res,next){
+  let mapId = "582a7ed422733e48e07efd07" //nannti ganti req.params.id
+    Maps.findOne({_id:mapId},(err,data) => {
+      res.json(data)
     })
 }
