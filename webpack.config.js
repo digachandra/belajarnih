3c5cad3707ff701a12f69534421d6ed47970f4a0
ariadiprana <ari.adiprana@gmail.com>
2016-11-15 14:57:32 +0700

    delete unused line in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ const webpack = require('webpack')
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
-    './index'
+    'webpack-hot-middleware/client'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
