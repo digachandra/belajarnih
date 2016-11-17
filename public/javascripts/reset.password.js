2f8d880bd5546fb16650491a1162c498227bd519
digachandra <imdigachandra@gmail.com>
2016-11-18 00:20:32 +0700

    Update button reset password form

--- public/javascripts/reset.password.js
+++ public/javascripts/reset.password.js
@@ -19,7 +19,7 @@ var ResetForm = React.createClass({
           <label htmlFor="confirm">Confirm Password</label>
           <input type="password" name="confirm" value={this.state.confirm} id="confirm" placeholder="Confirm password" className="form-control" onChange = {this.inputConfirmChange}/>
         </div>
-        <input type="submit" className="btn btn-success" value='Change Password'/>
+        <input type="submit" className="btn btn-block btn-warning" value='Change Password'/>
         <div className="form-action text-center">
           <a href="/login">Direct me to login page</a>
         </div>
