09303cbed4372c18663fd7d045a9904f938e47df
digachandra <imdigachandra@gmail.com>
2016-11-17 19:33:25 +0700

    Update skeleton & script page forgot password

--- public/javascripts/forgot.password.js
+++ public/javascripts/forgot.password.js
@@ -7,14 +7,16 @@ var ForgotPasswordForm = React.createClass({
   },
   render: function(){
     return(
-      <div className="form-group">
-        <label for="txt-email">Email</label>
-        <input id="txt-email" type="email" className="form-control" onChange = {this.inputEmailChange} autocomplete="off" placeholder="your@email.com" autofocus required />
-      </div>
-      <button type="submit" className="btn btn-warning btn-block">SUBMIT</button>
-      <div className="form-action text-center">
-        <a href="/login">Already have an account?</a>
-      </div>
+      <form id="content-form">
+        <div className="form-group">
+          <label for="txt-email">Email</label>
+          <input id="txt-email" type="email" className="form-control" onChange = {this.inputEmailChange} autocomplete="off" placeholder="your@email.com" autofocus required />
+        </div>
+        <button type="submit" className="btn btn-warning btn-block">SUBMIT</button>
+        <div className="form-action text-center">
+          <a href="/login">Already have an account?</a>
+        </div>
+      </form>
     )
   }
 })
